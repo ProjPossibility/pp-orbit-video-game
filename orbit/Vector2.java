3599f06a48281f8d5f012d7c5c7b232f3bd6d9e0
binputgame0team <binputgame0team@26530590-679a-11de-80b8-55702d16276a>
2007-11-17 21:27:50 +0000

    Added vector class

---
+++ orbit/Vector2.java
@@ -0,0 +1,51 @@
+/*
+	This is the useful 2D vector struct. Everything's public
+	to make things easy
+
+*/
+
+import java.Math.*;
+
+package orbit;
+
+public class Vector2 {
+
+	public double x;
+	public double y;
+
+	public Vector2() {
+		x = 0.0;
+		y = 0.0;
+	}
+
+	public Vector2(double x,double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getLength() {
+		return Math.sqrt(x*x + y*y);
+	}
+
+	public Vector2 getNormalized() {
+		Vector2 v = new Vector2(x/getLength(),y/getLength());
+		return v;
+	}
+
+	public Vector2 addVector(Vector2 v) {
+		Vector2 v2 = new Vector2(x + v.x,y+v.y);
+		return v2;
+	}
+
+	public Vector2 subVector(Vector2 v) {
+		return new Vector2(x - v.x,y - v.y);
+	}
+
+	public double dot(Vector2 v) {
+		return x*v.x + y*v.y;
+	}
+
+	public Vector2 scale(double s) {
+		return new Vector2(x*s,y*s);
+	}
+}
